291f1a42bb64d48f3f3aafd08f508f04b8fbc967
maranzan <nino.maranzana@etu.univ-grenoble-alpes.fr>
2025-03-24 09:20:52 +0100

    test

--- rl-lib/src/main/java/fr/polytech/mnia/App.java
+++ rl-lib/src/main/java/fr/polytech/mnia/App.java
@@ -4,8 +4,8 @@ public class App
 {
     public static void main( String[] args ) throws Exception{           
         
-        SimpleRunner sr = new SimpleRunner() ;
-        sr.execSequence() ; 
+        //SimpleRunner sr = new SimpleRunner() ;
+        //sr.execSequence() ; 
 
         // SchedulerRunner sc = new SchedulerRunner() ;
         // sc.execSequence();
