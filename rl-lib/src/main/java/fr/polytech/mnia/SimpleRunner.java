2b6294ceb41f507b9363b5660718fc470706e112
maranzan <nino.maranzana@etu.univ-grenoble-alpes.fr>
2025-04-07 08:59:02 +0200

    ajout bGradient

--- rl-lib/src/main/java/fr/polytech/mnia/SimpleRunner.java
+++ rl-lib/src/main/java/fr/polytech/mnia/SimpleRunner.java
@@ -9,7 +9,6 @@ import de.prob.statespace.Transition;
  * Cette classe illustre l'exécution de SimpleRL.mch
  */
 public class SimpleRunner extends Runner{
-    double epsilon = 0.2; // Exploration rate
     Evironnement env;
     List<Transition> choices;
     int iterations = 1000;
@@ -31,6 +30,11 @@ public class SimpleRunner extends Runner{
     
     @Override
     public void execSequence() throws Exception {
+        eGreedy();
+        bGradient();
+    }
+
+    public void eGreedy(){
         int choice;
         double reward;
         Transition t;
@@ -58,8 +62,8 @@ public class SimpleRunner extends Runner{
             }
             //showTransition(t);
             //animator.printState(env.getState());
-            System.out.println("\nChoosen "+t.getParameterPredicate());        //////////
-            System.out.println("Reward : "+reward);
+            //System.out.println("\nChoosen "+t.getParameterPredicate());        //////////
+            //System.out.println("Reward : "+reward);
             totalReward[choice] += reward;
             TimesChoosen[choice]++;
             results[choice] = totalReward[choice] / TimesChoosen[choice];
@@ -70,10 +74,10 @@ public class SimpleRunner extends Runner{
             System.out.println("Choosen : "+TimesChoosen[i]+" times");
             System.out.println("Expected result : "+results[i]);
         }
-        
     }
 
     public int choose(Double[] results){
+        double epsilon = 0.2;
         Random random = new Random();
         double alea = random.nextDouble();
         int choice;
@@ -99,7 +103,69 @@ public class SimpleRunner extends Runner{
         return index;
     }
 
+    public void bGradient(){
+        int choice;
+        double reward;
+        double alpha = 0.01;
+        Transition t;
     
-
+        Double[] results = new Double[choices.size()];
+        int[] TimesChoosen = new int[choices.size()];
+    
+        for(int i = 0; i < choices.size(); i++){
+            results[i] = 1.0 / results.length;
+            TimesChoosen[i] = 0;
+        }
+    
+        for(int i = 0; i < iterations; i++){
+            choice = chooseGradient(results);
+            t = choices.get(choice);
+            env.runAction(t);
+    
+            if(env.getState().getStateRep().equals("( res=OK )")){
+                reward = 1.0;
+            } else {
+                reward = 0.0;
+            }
+    
+            TimesChoosen[choice]++;
+            for (int j = 0; j < results.length; j++) {
+                if(j == choice){
+                    results[choice] = results[choice] + alpha * reward * (1 - results[choice]);
+                } else {
+                    results[j] = results[j] - alpha * reward * results[j];
+                }
+            }
+    
+            // Normalisation des probabilités
+            double sum = 0.0;
+            for (int j = 0; j < results.length; j++) {
+                sum += results[j];
+            }
+            for (int j = 0; j < results.length; j++) {
+                results[j] /= sum;
+            }
+        }
+    
+        for (int i = 0; i < results.length; i++) {
+            System.out.println("\nfor " + choices.get(i).getParameterPredicate());
+            System.out.println("Choosen : " + TimesChoosen[i] + " times");
+            System.out.println("Probability : " + results[i]);
+        }
+    }
+    
+    public int chooseGradient(Double[] results){
+        Random random = new Random();
+        double alea = random.nextDouble();
+        double cumulativeProbability = 0.0;
+        for(int i = 0; i < results.length; i++){
+            cumulativeProbability += results[i];
+            if(alea < cumulativeProbability){
+                return i;
+            }
+        }
+        return results.length - 1;
+    }
+    
     
 }
